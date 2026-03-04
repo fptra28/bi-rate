7d6f2fb0e8432db0da442fac03fb314376a4c5c7
fptra28 <faturrahman86.fr@gmail.com>
2026-03-04 11:23:26 +0700

    update:API Token

--- server.js
+++ server.js
@@ -7,8 +7,29 @@ const biRateRouter = require("./routes/biRate");
 
 const app = express();
 const PORT = Number(process.env.PORT || 5000);
+const API_TOKEN = process.env.BI_RATE_API_TOKEN || "";
 
 app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+    if (!API_TOKEN) {
+        return res.status(500).json({
+            error: "Konfigurasi token belum diatur",
+            message: "Set env BI_RATE_API_TOKEN",
+        });
+    }
+
+    const auth = req.headers.authorization || "";
+    const [type, token] = auth.split(" ");
+    if (type !== "Bearer" || token !== API_TOKEN) {
+        return res.status(401).json({
+            error: "Unauthorized",
+            message: "Bearer token tidak valid",
+        });
+    }
+
+    return next();
+});
 
 startCron();
 
